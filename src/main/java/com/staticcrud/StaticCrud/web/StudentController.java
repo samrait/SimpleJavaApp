b5b5972f1edf9225257f756d19c0f7ea8631834f
Shishir Meshram <shishir.meshram@perficient.com>
2020-06-11 14:13:42 +0530

    comment added into controller

--- src/main/java/com/staticcrud/StaticCrud/web/StudentController.java
+++ src/main/java/com/staticcrud/StaticCrud/web/StudentController.java
@@ -17,7 +17,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.staticcrud.StaticCrud.model.Course;
 import com.staticcrud.StaticCrud.service.StudentService;
 
-
+//Controller
 @RestController
 public class StudentController {
 
